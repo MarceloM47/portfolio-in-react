68eaf63c70bd440184ce0233323383f880d26009
Marcelo Morinigo <78925750+MarceloM47@users.noreply.github.com>
2023-02-26 18:29:31 +0000

    pequeños cambios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,7 @@ export default function Navbar() {
                 <Link to='/skills'>Skills</Link>
             </li>
         </ul>
+
     </nav>
 </header>
   )
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 
 export default function HomePage() {
+
     return (
         
         <section id="title">
@@ -16,9 +17,9 @@ export default function HomePage() {
         </div>
 
         <div className="redes">
-            <a className="red" target={"_blank"} href="https://github.com/marcelom47"><img src="/svg/github.svg" alt="github"/></a>
-            <a className="red" target={"_blank"} href="https://www.linkedin.com/in/marcelo-morinigo-74808b213/"><img src="/svg/linkedin.svg" alt="linkedin"/></a>
-            <a className="red" target={"_blank"} href="mailto:morinigomarcelo40@gmail.com"><img src="/svg/mail.svg" alt="mail"/></a>
+            <a className="red" target={"_blank"} rel="noreferrer" href="https://github.com/marcelom47"><img src="/svg/github.svg" alt="github"/></a>
+            <a className="red" target={"_blank"} rel="noreferrer" href="https://www.linkedin.com/in/marcelo-morinigo-74808b213/"><img src="/svg/linkedin.svg" alt="linkedin"/></a>
+            <a className="red" target={"_blank"} rel="noreferrer" href="mailto:morinigomarcelo40@gmail.com"><img src="/svg/mail.svg" alt="mail"/></a>
         </div>
         </section>
     )
